39a4d4988b761c6490c0e316f910bccf1b6334b0
marcyto <43577419+marcyto@users.noreply.github.com>
2023-04-05 23:03:36 -0300

    commit version 2

--- src/assets/Banner/index.jsx
+++ src/assets/Banner/index.jsx
@@ -1,7 +1,7 @@
 
 import background1 from '../../assets/Banner/background1.png';
 import background2 from '../../assets/Banner/background2.png';
-import background3 from '../../assets/Banner/Background3.png';
+import background3 from '../../assets/Banner/background3.png';
 
 export const imageBanner = [
     {
