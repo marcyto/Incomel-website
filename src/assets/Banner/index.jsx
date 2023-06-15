09ac6d64bcc0b54267a9893d8d6734499f51f593
Marcio Lima <43577419+marcyto@users.noreply.github.com>
2023-06-15 15:18:05 -0300

    Update index.jsx last version promo background banner

--- src/assets/Banner/index.jsx
+++ src/assets/Banner/index.jsx
@@ -2,7 +2,7 @@
 import background1 from '../../assets/Banner/background1.png';
 import background2 from '../../assets/Banner/background2.png';
 import background3 from '../../assets/Banner/background3.png';
-import backgroundpromo from '../../assets/Banner/backgroud4.png';
+import backgroundpromo from '../../assets/Banner/background4.png';
 
 export const imageBanner = [
     {
