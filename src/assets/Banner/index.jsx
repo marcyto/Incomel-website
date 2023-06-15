6c241f7660870d1f06ce53de30334c44283f3315
Marcio Lima <43577419+marcyto@users.noreply.github.com>
2023-06-15 10:54:17 -0300

    Update banner promo junho

--- src/assets/Banner/index.jsx
+++ src/assets/Banner/index.jsx
@@ -2,8 +2,12 @@
 import background1 from '../../assets/Banner/background1.png';
 import background2 from '../../assets/Banner/background2.png';
 import background3 from '../../assets/Banner/background3.png';
+import background4 from '../../assets/Banner/background4.png';
 
 export const imageBanner = [
+    {
+        image:background4
+    },
     {
         image:background1
     },
