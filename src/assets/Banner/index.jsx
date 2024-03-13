cce884288ddbc4f38a501d75406be75d7206d119
Marcio Lima <43577419+marcyto@users.noreply.github.com>
2024-03-13 09:42:32 -0300

    Update banner index.jsx

--- src/assets/Banner/index.jsx
+++ src/assets/Banner/index.jsx
@@ -2,13 +2,10 @@
 import background1 from '../../assets/Banner/background1.png';
 import background2 from '../../assets/Banner/background2.png';
 import background3 from '../../assets/Banner/background3.png';
-import bannerimg2 from '../../assets/Banner/banner2.jpg';
+
 
 
 export const imageBanner = [
-    {
-        image:bannerimg2
-    },
     {
         image:background1
     },
